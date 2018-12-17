909f263a4dfd02f4a2e4863d651d3a73bd83f107
Matthew Hughes <matt.hughes@live.com>
2018-12-17 10:40:38 +0100

    Added AutoRotator base (need to convert to runtime node)

---
+++ AutoRotator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotator : MonoBehaviour
+{
+
+    public Vector3 Speed = new Vector3(20f, 30f, 50f);
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Time.deltaTime * Speed.x,
+			Time.deltaTime * Speed.y,
+			Time.deltaTime * Speed.z);
+    }
+}
