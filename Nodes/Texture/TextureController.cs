0654d01b4e05122322ab65d1d6329d2c9e1d1fab
Matthew Hughes <matt.hughes@live.com>
2018-12-17 10:40:00 +0100

    Added texture offset control

--- Nodes/Texture/TextureController.cs
+++ Nodes/Texture/TextureController.cs
@@ -11,16 +11,22 @@ public class TextureController : RuntimeNode
     public Material Material;
     [Input(ShowBackingValue.Always, ConnectionType.Override)] public float ScaleX;
     [Input(ShowBackingValue.Always, ConnectionType.Override)] public float ScaleY;
+    [Input(ShowBackingValue.Always, ConnectionType.Override)] public float OffsetX;
+    [Input(ShowBackingValue.Always, ConnectionType.Override)] public float OffsetY;
 
     Vector2 MainTextureScale = new Vector2();
+    Vector2 MainTextureOffset = new Vector2();
 
     public override void Update()
     {
         MainTextureScale.x = ScaleX = GetInputValue<float>("ScaleX");
         MainTextureScale.y = ScaleY = GetInputValue<float>("ScaleY");
+        MainTextureOffset.x = OffsetX = GetInputValue<float>("OffsetX");
+        MainTextureOffset.y = OffsetY = GetInputValue<float>("OffsetY");
         try
         {
             Material.SetTextureScale("_MainTex", MainTextureScale);
+            Material.SetTextureOffset("_MainTex", MainTextureOffset);
         }
         catch (UnassignedReferenceException exception)
         {
