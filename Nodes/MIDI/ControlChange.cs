29722fb684d7312a5bebbff32825b62a95892ddb
Matthew Hughes <matt.hughes@live.com>
2019-02-08 16:03:40 +0100

    Started ControlChange node

--- Nodes/MIDI/ControlChange.cs
+++ Nodes/MIDI/ControlChange.cs
@@ -1,3 +1,6 @@
+using Midi;
+using Eidetic;
+using Eidetic.Confluence;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,43 +8,53 @@ using System.Linq;
 using System.Reflection;
 using UnityEngine;
 using XNode;
-using MidiJack;
-using Eidetic.Confluence;
 
 [CreateNodeMenu("MIDI/ControlChange"),
     NodeTint(Colors.ExternalInputTint)]
 public class ControlChange : RuntimeNode
 {
-    public MidiChannel Channel;
+    public String DeviceName;
+    public InputDevice Device { get; private set; }
+    public Midi.Channel Channel;
     public int Number;
     public bool BiDirectional;
-    [Output(ShowBackingValue.Never)] public int Int;
-    [Output(ShowBackingValue.Never)] public float Float;
+    [Output(ShowBackingValue.Always)] public int Int;
+    [Output(ShowBackingValue.Always)] public float Float;
 
-    public override void Update()
+    protected override void Init()
     {
-        Debug.Log(MidiMaster.GetKnob(Channel, Number));
+        Debug.Log("init");
+        foreach (InputDevice inputDevice in InputDevice.InstalledDevices)
+        {
+            if (inputDevice.Name.ToLower().Equals(DeviceName.ToLower()))
+            {
+                Device = inputDevice;
+                break;
+            }
+        }
+        if (Device != null)
+        {
+            Device.Open();
+            Device.StartReceiving(null);
+            Device.ControlChange += UpdateValues;
+            Debug.LogFormat("Opened MIDI Device: {0}", Device.Name);
+        }
     }
 
-    public override object GetValue(NodePort port)
+    void UpdateValues(ControlChangeMessage m)
     {
-        switch (port.fieldName)
+        if (m.Channel == Channel)
         {
-            case "Int":
+            if (m.Control.Number() == Number)
+            {
+                Int = m.Value;
+                Float = (m.Value / 127f);
+                if (BiDirectional)
                 {
-                    if (BiDirectional)
-                        return Mathf.RoundToInt(MidiMaster.GetKnob(Channel, Number) * 127) - 64;
-                    else
-                        return Mathf.RoundToInt(MidiMaster.GetKnob(Channel, Number) * 127);
-                };
-            case "Float":
-                {
-                    if (BiDirectional)
-                        return MidiMaster.GetKnob(Channel, Number) - .5f;
-                    else
-                        return MidiMaster.GetKnob(Channel, Number);
+                    Int -= 64;
+                    Float -= .5f;
                 }
-            default: return null;
+            }
         }
     }
 }
